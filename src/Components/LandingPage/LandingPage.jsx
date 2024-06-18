24c191fa638071169586136bc82a14b5f5195377
vrushang1234 <vrushanganand20@gmail.com>
2024-06-18 15:37:09 -0700

    Added loading animation and changed z-index for socials

--- src/Components/LandingPage/LandingPage.jsx
+++ src/Components/LandingPage/LandingPage.jsx
@@ -6,9 +6,16 @@ export default function Landing({ setstart }) {
     document.getElementById("zoom-v").classList.add("zoom-animate");
     document.getElementsByClassName("landing-button")[0].style.display = "None";
     document.getElementsByClassName("landing-page")[0].classList.add("outro");
+    setTimeout(() => {
+      document.getElementsByClassName("loader")[0].style.opacity = "1";
+    }, 1500);
+    setTimeout(() => {
+      document.getElementsByClassName("loader")[0].style.opacity = "0";
+    }, 10000);
   }
   return (
     <div className="landing-page">
+      <div className="loader"></div>
       <div className="landing-text">
         <div style={{ overflow: "visible" }}>
           <span id="zoom-v">Vrushang</span>
