2e331014760727ecf6804f7550ad697fade21540
vrushang1234 <vrushanganand20@gmail.com>
2026-02-26 23:24:23 -0800

    Added hftsystem article

---
+++ app/articles/hft-system/page.js
@@ -0,0 +1,8 @@
+import "../indiv-article.css";
+export default function HFTSystem() {
+    return (
+        <div className="indiv-article">
+            HFT System Article Under Construction
+        </div>
+    );
+}
