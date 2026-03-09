8fed01594d7fb8684d8c12b7c65715ec0c1bc405
vrushang1234 <vrushanganand20@gmail.com>
2026-03-09 14:08:52 -0700

    Header for hft articles under construction

--- app/articles/hft-system/page.js
+++ app/articles/hft-system/page.js
@@ -2,7 +2,7 @@ import "../indiv-article.css";
 export default function HFTSystem() {
     return (
         <div className="indiv-article">
-            HFT System Article Under Construction
+            <h1>HFT System Article Under Construction</h1>
         </div>
     );
 }
