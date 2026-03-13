48dafff0fba87ebe18d752483636724c48f3a18a
vrushang1234 <vrushanganand20@gmail.com>
2026-03-12 21:50:18 -0700

    Added redirect to hft-system-report for 159B

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,15 @@
 const nextConfig = {
     /* config options here */
     reactCompiler: true,
+    async redirects() {
+        return [
+            {
+                source: '/articles/hft-system',
+                destination: '/assets/EECS_159B_HFT_Report.pdf',
+                permanent: false,
+            },
+        ];
+    },
 };
 
 export default nextConfig;
